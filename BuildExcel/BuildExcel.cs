b2811098ce5bfb6a7dad7b192a6300dbfeefb9b7
idoku <dragon.ice.995@hotmail.com>
2016-05-04 16:23:40 +0800

    init

---
+++ BuildExcel/BuildExcel.cs
@@ -0,0 +1,275 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildExcel
+{
+    public class BuildExcel 
+    {
+        public BuildExcel()
+        {
+            this.workbook = new HSSFWorkbook();
+            currentSheet = (HSSFSheet)this.workbook.CreateSheet("sheet1");
+            this.workbook.CreateSheet("sheet2");
+            this.workbook.CreateSheet("sheet3");
+
+        }
+
+
+
+        public BuildExcel(Stream fileStream)
+        {
+            workbook = new HSSFWorkbook(fileStream);
+            this.currentSheet = (HSSFSheet)this.workbook.GetSheetAt(0);
+        }
+
+
+        /// <summary>
+        /// NPOI文档流
+        /// </summary>
+        private HSSFWorkbook workbook = null;
+
+        /// <summary>
+        /// 当前操作页
+        /// </summary>
+        private HSSFSheet currentSheet = null;
+
+        /// <summary>
+        /// 当前单元
+        /// </summary>
+        private HSSFCell currentCell = null;
+        
+        /// <summary>
+        /// 单元样式
+        /// </summary>
+        private ICellStyle cellStyle = null;
+
+
+        #region 页操作
+        /// <summary>
+        /// 选择操作页
+        /// </summary>
+        /// <param name="sheetName"></param>
+        public void SelectSheet(string sheetName)
+        {
+            currentSheet = (HSSFSheet)workbook.GetSheet(sheetName);          
+        }
+        #endregion
+
+        #region public
+        /// <summary>
+        /// 获取Excel文件流
+        /// </summary>
+        /// <returns></returns>
+        public Stream GetStream()
+        {
+            MemoryStream stream = new MemoryStream();
+            workbook.Write(stream);
+            stream.Position = 0;
+            return stream;
+        }
+
+        [ConditionalAttribute("DEBUG")]
+        public void PrintCurrentSheet()
+        {
+            for (int rowIndex = 0; rowIndex < currentSheet.LastRowNum; rowIndex++)
+            {
+                IRow row = currentSheet.GetRow(rowIndex);
+                for (int cellIndex = 0; cellIndex < row.LastCellNum; cellIndex++)
+                {
+                    Console.WriteLine(row.GetCell(cellIndex).StringCellValue);
+                }
+            }
+        }
+        
+        public void Replace(string what, string replacement)
+        {
+            ICell cell = FindFirstCell(currentSheet, what);
+            if (cell != null)
+            {
+                cell.SetCellValue(replacement);
+            }
+        }
+
+        public void ReplaceInsertTable(string what, DataTable table)
+        {
+            ICell cell = FindFirstCell(currentSheet, what);
+            if (cell != null)
+            {
+                int sheetIndex = workbook.GetSheetIndex(currentSheet);
+                int rowCount = table.Rows.Count;
+                int rowIndex = cell.RowIndex;
+                InsertRows(currentSheet, rowIndex + 1, rowCount - 1);//remove what row
+                InsertDataTable(table, sheetIndex, rowIndex, false);                
+            }
+        }
+
+        public void SetBorder(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            for (int rowIndex = firstRow; rowIndex < lastRow; rowIndex++)
+            {
+                var row = HSSFCellUtil.GetRow(rowIndex, currentSheet);
+                for (int cellIndex = firstCol; cellIndex < lastCol; cellIndex++)
+                {
+                    var cell = HSSFCellUtil.GetCell(row, cellIndex);
+                    cell.CellStyle = GetThinBDRStyle();
+                }
+            }
+        }
+
+        private void SetBorderLeft(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            HSSFRegionUtil.SetBorderLeft(BorderStyle.THIN, new CellRangeAddress(firstRow, lastRow, firstCol, lastCol), currentSheet, workbook);
+        }
+
+        private void SetBorderRight(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            HSSFRegionUtil.SetBorderRight(BorderStyle.THIN, new CellRangeAddress(firstRow, lastRow, firstCol, lastCol), currentSheet, workbook);
+        }
+
+        private void SetBorderBottom(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            HSSFRegionUtil.SetBorderTop(BorderStyle.THIN, new CellRangeAddress(firstRow, lastRow, firstCol, lastCol), currentSheet, workbook);
+        }
+
+        private void SetBorderTop(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            HSSFRegionUtil.SetBorderTop(BorderStyle.THIN, new CellRangeAddress(firstRow, lastRow, firstCol, lastCol), currentSheet, workbook);
+        }
+
+        private ICellStyle GetThinBDRStyle()
+        {         
+            ICellStyle style = workbook.CreateCellStyle();
+            style.BorderRight = BorderStyle.THIN;
+            style.BorderBottom = BorderStyle.THIN;
+            style.BorderLeft = BorderStyle.THIN;
+            style.BorderTop = BorderStyle.THIN;
+            return style;
+        }
+
+        public void MergedRegion(int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+            currentSheet.AddMergedRegion(new CellRangeAddress(firstRow, lastRow, firstCol, lastCol));
+        }
+
+        #endregion
+
+    
+
+     
+
+
+        #region DataTable helper
+
+        
+
+        public void InsertDataTable(DataTable table)
+        {
+            InsertDataTable(table, 0, 0, true);
+        }
+
+        public void InsertDataTable(DataTable table, int sheetIndex,int rowIndex,bool hasHeader)
+        {
+            string sheetName = workbook.GetSheetName(sheetIndex);
+            var sheet = workbook.GetSheet(sheetName);
+            var style = GetThinBDRStyle();
+            if (hasHeader)
+            {
+                var headerRow = sheet.CreateRow(rowIndex);
+                foreach (DataColumn column in table.Columns)
+                {
+                    var headerCell = headerRow.CreateCell(column.Ordinal);
+                    headerCell.SetCellValue(column.ColumnName);
+                    headerCell.CellStyle = style;
+                }
+                rowIndex++;
+            }            
+            foreach (DataRow row in table.Rows)
+            {
+                var dataRow = sheet.CreateRow(rowIndex++);
+                
+                foreach (DataColumn column in table.Columns)
+                {
+                    var cell = dataRow.CreateCell(column.Ordinal);
+                    cell.SetCellValue(row[column].ToString());
+                    cell.CellStyle = style;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 插入行
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="fromRowIndex"></param>
+        /// <param name="rowCount"></param>
+        private void InsertRows(ISheet sheet, int fromRowIndex, int rowCount)
+        {
+            sheet.ShiftRows(fromRowIndex, sheet.LastRowNum, rowCount, false, true);
+            for (int rowIndex = fromRowIndex; rowIndex < fromRowIndex+rowCount; rowIndex++)
+            {
+                IRow rowSource = sheet.GetRow(rowIndex + rowCount);
+                IRow rowInsert = sheet.CreateRow(rowIndex);
+                rowInsert.Height = rowSource.Height;
+                for (int colIndex = 0; colIndex < rowSource.LastCellNum; colIndex++)
+                {
+                    ICell cellSource = rowSource.GetCell(colIndex);
+                    ICell cellInsert = rowInsert.CreateCell(colIndex);
+                    if (cellSource != null)
+                    {
+                        cellInsert.CellStyle = cellSource.CellStyle;
+                    }
+                }
+            }
+        }
+        #endregion
+
+     
+
+
+    
+
+        #region 
+        private void SetCellBorder(ISheet sheet, int firstRow, int lastRow, int firstCol, int lastCol)
+        {
+
+            for (int rowIndex = 0; rowIndex < sheet.LastRowNum; rowIndex++)
+            {
+                IRow row = sheet.GetRow(rowIndex);
+                for (int cellIndex = 0; cellIndex < row.LastCellNum; cellIndex++)
+                {
+                    ICell cell = row.GetCell(cellIndex);
+                    cell.CellStyle = GetThinBDRStyle();
+                }
+            }
+        }
+
+        private ICell FindFirstCell(ISheet sheet, string text)
+        {                      
+            for (int rowIndex = 0; rowIndex < sheet.LastRowNum; rowIndex++)
+            {
+                IRow row = sheet.GetRow(rowIndex);
+                for (int cellIndex = 0; cellIndex < row.LastCellNum; cellIndex++)
+                {
+                    ICell cell = row.GetCell(cellIndex);
+                    if (cell.StringCellValue.Equals(text))
+                    {
+                        return cell;
+                    }
+                }
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
---
+++ BuildExcel/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NPOI.HSSF.UserModel;
+using System.IO;
+using System.Data;
+using NPOI.SS.UserModel;
+
+namespace BuildExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertDataTable();
+            Console.Read();
+        }
+
+
+        public static void DictionaryTable()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>() { 
+            { "楼盘名称","123" },
+            { "楼盘地址","321" },
+            {"楼栋名称","123"},
+            {"楼栋地址","123"},
+            {"楼栋号","123"}
+            };
+
+            var dt = DictionaryToTable(dic, 3);
+            BuildExcel excel = new BuildExcel();
+            excel.InsertDataTable(dt);
+            Stream ms = excel.GetStream();
+            FileStream file = new FileStream("a.xls", FileMode.Create);
+            ms.CopyTo(file);
+            file.Close();
+        }
+
+        public static DataTable DictionaryToTable(Dictionary<string, string> dictionary, int dicToColumnNum)
+        {
+            int colNum = dicToColumnNum * 2;
+            int rowNum = dictionary.Count() / dicToColumnNum;
+            DataTable dt = CreateTable(rowNum, colNum);
+
+            if (dicToColumnNum <= 0)
+            {
+                throw new ArgumentOutOfRangeException("dicToColumnNum", "字典展开不能小于等于0.");
+            }
+            else
+            {
+                for (int i = 0, col = 0, row = 0; i < dictionary.Count(); i++)
+                {
+                    var item = dictionary.ElementAt(i);
+                    col = 2 * (i % dicToColumnNum);
+                    row = i / dicToColumnNum;
+                    dt.Rows[row][col] = item.Key;
+                    dt.Rows[row][col + 1] = item.Value;
+                }
+            }
+            return dt;
+        }
+
+        private static DataTable CreateTable(int rowNums, int colNums)
+        {
+            DataTable table = new DataTable();
+            for (int i = 0; i < colNums; i++)
+            {
+                DataColumn dc = new DataColumn();
+                table.Columns.Add(dc);
+            }
+            for (int j = 0; j <= rowNums; j++)
+            {
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        public static void InsertDataTable() {
+
+            DataTable dt = CreateTable(3,8);
+            FileStream file = new FileStream(@"Excel/template.xls", FileMode.Open, FileAccess.ReadWrite);
+            BuildExcel excel = new BuildExcel(file);
+         
+            excel.ReplaceInsertTable("《查勘信息》", dt);
+
+            excel.SetBorder(0, 0, 1, 1);
+            Stream ms = excel.GetStream();                      
+            FileStream saveTo = new FileStream("b.xls", FileMode.Create);
+            ms.CopyTo(saveTo);
+            saveTo.Close();
+            file.Close();
+        }
+
+        public static void TransDataTable()
+        {
+            DataTable dt = new DataTable("cart");
+            DataColumn dc1 = new DataColumn("prizename", Type.GetType("System.String"));
+            DataColumn dc2 = new DataColumn("point", Type.GetType("System.Int16"));
+            DataColumn dc3 = new DataColumn("number", Type.GetType("System.Int16"));
+            DataColumn dc4 = new DataColumn("totalpoint", Type.GetType("System.Int64"));
+            DataColumn dc5 = new DataColumn("prizeid", Type.GetType("System.String"));
+            DataColumn dc6 = new DataColumn("prizeid2", Type.GetType("System.String"));
+            DataColumn dc7 = new DataColumn("prizeid3", Type.GetType("System.String"));
+            DataColumn dc8 = new DataColumn("prizeid4", Type.GetType("System.String"));
+            dt.Columns.Add(dc1);
+            dt.Columns.Add(dc2);
+            dt.Columns.Add(dc3);
+            dt.Columns.Add(dc4);
+            dt.Columns.Add(dc5);
+            dt.Columns.Add(dc6);
+            dt.Columns.Add(dc7);
+            dt.Columns.Add(dc8);
+            //以上代码完成了DataTable的构架，但是里面是没有任何数据的
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["prizename"] = "娃娃";
+                dr["point"] = 10;
+                dr["number"] = 1;
+                dr["totalpoint"] = 10;
+                dr["prizeid"] = "001";
+                dr["prizeid2"] = "002";
+                dr["prizeid3"] = "003";
+                dr["prizeid4"] = "004";
+                dt.Rows.Add(dr);
+            }
+
+            BuildExcel excel = new BuildExcel();
+          
+            excel.InsertDataTable(dt);
+            Stream ms = excel.GetStream();
+            FileStream file = new FileStream("b.xls", FileMode.Create);
+            ms.CopyTo(file);
+            file.Close();
+
+        }
+    }
+}
